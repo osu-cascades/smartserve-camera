3fa092edd8d626ea3b204b3a6c9c4a47f3fbba49
teemal <taymal1987@gmail.com>
2018-01-20 19:36:20 -0800

    added resizing tool and test image

---
+++ imtools.py
@@ -0,0 +1,11 @@
+#Taylor Mallory
+#tools working with openCV
+
+from PIL import Image
+from numpy import *
+
+def imresize(im,sz):
+	""" Resize an image array using PIL. """
+	pil_im = Image.fromarray(uint8(im))
+	return array(pil_im.resize(sz))
+
