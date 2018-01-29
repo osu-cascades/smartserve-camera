3f6624ed3b389b30bbec8277601f7d5ceb35bc47
teemal <taymal1987@gmail.com>
2018-01-28 16:58:00 -0800

    open image using iostream

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 #Taylor Mallory
 #testies
+from PIL import Image
+from pylab import *
+import rof
+
+im = array(Image.open('test_img.jpg').convert('L'))
+U,T = rof.denoise(im,im)
+figure()
+gray()
+imshow(U)
+axis('equal')
+axis('off')
+show()
