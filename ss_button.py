79f36887058a18b072f8bd6a44e70cda061bae0e
Tee Mal <taymal1987@gmail.com>
2018-02-04 04:02:42 +0000

    fix time as img name

--- ss_button.py
+++ ss_button.py
@@ -3,18 +3,11 @@ import time
 import ss_camera
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.OUT)
 
-try:
-	while True:
-		button_state = GPIO.input(23)
-		if button_state == False:
-			GPIO.output(24, True)
-			ss_camera.take_pic()
-			time.sleep(2.0)
-		else:
-			GPIO.output(24, False)
-except:
-	GPIO.cleanup()
+while True:
+	button_state = GPIO.input(24)
+	if button_state == False:
+		ss_camera.take_pic()
+		time.sleep(.2)
