59c800cd31a854e7078c4e0db3777d477b4f3287
JWTappert <justin@theflybook.com>
2018-05-13 22:17:01 -0700

    increase count down for preview and capture

--- scripts/camera-daemon.py
+++ scripts/camera-daemon.py
@@ -26,7 +26,7 @@ def capture(image=null):
     image2 = capture_stream(True)
     diff = compute_diff(image1, image2)
     insert(diff)
-	print("Structural difference: %s %%" % (diff*100))
+    print("Structural difference: %s %%" % (diff*100))
 
 
 def prepare_camera(preview):
@@ -37,6 +37,10 @@ def prepare_camera(preview):
         # these print statements help for debugging as well as
         # allowing time for the camera to warm up
         print("taking picture in")
+        print("5")
+        sleep(1)
+        print("4")
+        sleep(1)
         print("3")
         sleep(1)
         print("2")
